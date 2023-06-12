782e20b93d92c49f837175e98f496e7d8b7dea99
Panda_Tine <92630250+christineilagan02@users.noreply.github.com>
2023-06-12 18:10:11 +0800

    Enhanced Parenthesis Checker program with colored output.

--- ParenthesisChecker.java
+++ ParenthesisChecker.java
@@ -1,17 +1,27 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class ParenthesisChecker {
+
+    // Color codes for console output
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_RED = "\u001B[31m";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         scanner.nextLine(); // Consume the newline character after reading N
 
+        System.out.println(ANSI_CYAN + "Parenthesis Checking Results:" + ANSI_RESET);
+
         for (int i = 0; i < N; i++) {
             String parentheses = scanner.nextLine();
             if (isBalanced(parentheses)) {
-                System.out.println("BALANCED");
+                System.out.println(formatResult(parentheses, ANSI_GREEN + "BALANCED" + ANSI_RESET));
             } else {
-                System.out.println("NOT BALANCED");
+                System.out.println(formatResult(parentheses, ANSI_RED + "NOT BALANCED" + ANSI_RESET));
             }
         }
     }
